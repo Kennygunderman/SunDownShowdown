a58efe61cfb3c374e136b0de494f5143ac37fa7c
Kenny <kennygunderman@gmail.com>
2019-07-20 15:32:58 -0400

    Add plugin files

---
+++ src/main/java/com/kgeezy/sundownshowdown/SunDownShowDown.kt
@@ -0,0 +1,9 @@
+package com.kgeezy.sundownshowdown
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SunDownShowDown: JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+}
